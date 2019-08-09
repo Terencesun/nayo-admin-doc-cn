231b1e0fcbf5eedaf0ef2c69c44ac2ca3732320f
Terence孫 <terence@segofun.com>
2019-08-10 00:15:23 +0800

    update

--- src/teadocs.config.js
+++ src/teadocs.config.js
@@ -16,7 +16,7 @@ module.exports = {
         headHtml: "",
         footHtml: "",
         isMinify: true,
-        rootPath: "/nayo-admin-doc-cn"
+        rootPath: "/nayo-admin-doc-cn/"
     },
     nav: {
         tree: "./tree"
